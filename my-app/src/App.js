a3d81b7dded055d2bfcae05066d4245ded60dd56
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-06 17:31:05 +0200

    add a footer with dynamic contact link

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -125,6 +125,20 @@ function App() {
           </div>
         </div>
       </main>
+      <div className="footer">
+        <div className="footer-container">
+          Deliveroo by Jules Sannequin <br />
+          contact me :
+          <a href="mailto:jules.sannequin@gmail.com">
+            jules.sannequin@gmail.com
+          </a>
+          <br />
+          Linkedin :
+          <a href="https://www.linkedin.com/in/jules-sannequin-1b680a250/">
+            Jules Sannequin
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
